30af209c1eae93f2fb038c2d5b0455a41f149a36
jinhyung999 <jinju8640108@naver.com>
2024-04-12 12:22:49 +0900

    end

---
+++ Chp5/grade.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef GUARD_grade_h
+#define GUARD_grade_h
+
+//grade.h
+#include <vector>
+#include <list>
+#include "Student_info.h"
+
+bool fgrade(const Student_info&);
+double grade(double, double, double); //header파일에서는 매개변수 이름 필요없음 그저 자료형만 표시 (더블, 더블, 더블)
+double grade(double, double, vector<double>&);
+double grade(const Student_info&);
+list<Student_info> extract_fails(list<Student_info>&);
+
+#endif
